3f4bce01277af171babb8c9afc0f71867d40199e
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-06 15:08:42 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,12 @@
+// copy a string using strcpy
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char str1[10]="Hello";
+  char str2[10];
+
+  printf("%s\n",strcpy(str2,str1));
+  printf("%s",str2);
+  return 0;
+}
